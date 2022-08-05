2f3f3e6fd4901591946cb34c28e10b1af09f76f4
tima <sergeyt@mackiev.com>
2022-08-05 09:37:10 +0300

    SS - debugging

--- T2TMessanger/Program.cs
+++ T2TMessanger/Program.cs
@@ -8,4 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+Console.WriteLine("SS");
+
 await builder.Build().RunAsync();
