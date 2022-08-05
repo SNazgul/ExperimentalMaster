e845358e306e39d3b714430bbaa3623bb8401b4a
tima <sergeyt@mackiev.com>
2022-08-05 11:49:42 +0300

    SS - Chnage 21

--- T2TMessanger/Program.cs
+++ T2TMessanger/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 Console.WriteLine("SS");
-Console.WriteLine("SS - 1");
+Console.WriteLine("SS - 12");
 
 await builder.Build().RunAsync();
