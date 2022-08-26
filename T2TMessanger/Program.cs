abc58fe07463cffea5927a63552b8003ed3f15a7
tima <sergeyt@mackiev.com>
2022-08-26 23:41:44 +0300

    AZ-51

--- T2TMessanger/Program.cs
+++ T2TMessanger/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 Console.WriteLine("SS - 31");
 Console.WriteLine("SS - 31");
 
-Console.WriteLine("AZ - 41");
+Console.WriteLine("AZ - 51");
 Console.WriteLine("AZ - 42");
 
 await builder.Build().RunAsync();
